f0a4700e31f680bf0ff49a92adc7118fa8d972dd
Sangkook Lee <sangkook.lee@solars.dev>
2023-01-23 21:14:58 +0900

    feat: Add helmet, cors, morgan

--- dwitter/server/app.js
+++ dwitter/server/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import tweetRouter from './router/tweet.js';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import tweetsRouter from './router/tweet.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
 
-app.use('/tweets', tweetRouter);
+app.use('/tweets', tweetsRouter);
 
 app.listen(8080);
